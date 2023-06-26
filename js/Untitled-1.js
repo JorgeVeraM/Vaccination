17d9fb4722d9cbd1d8e3b6da0f23882e0f30b6f3
JorgeVeraM <99235658+JorgeVeraM@users.noreply.github.com>
2023-06-26 17:43:18 -0600

    Add files via upload

--- js/Untitled-1.js
+++ js/Untitled-1.js
@@ -1,13 +1,20 @@
 
-function escribir(){
-    document.write("<p>There are emotional and behavioral consequences of stress that can make it difficult to perform your daily routine. </p>")
-};
 
 
+// menu
 
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0);
+}
+)
+
+var buttom = document.querySelector("#menu");
+buttom.addEventListener('click',() =>{
+    let menu = document.querySelector(".nav")
+    let header = document.querySelector("header");
+    menu.style.display = "block";
+    header.style.background = "rgba(255, 255, 255, .25)";
 })
 
 
@@ -15,12 +22,129 @@ window.addEventListener("scroll", function(){
 
 
 
-const menuPhone = document.querySelector(".menu-phone");
-const menu1 = document.querySelector(".menu1");
 
 
-menuPhone.addEventListener("click" , ()=> {
 
-    menu1.classList.toggle("activado");
+var boton1 = document.querySelector("#dash");
+
+boton1.addEventListener("click" , () => {
+    
+    var p_1 = document.querySelector("#p-1");
+
+    if(p_1.style.display == "block"){
+
+        document.querySelector("#viñeta").className = "fa-solid fa-angle-down";
+        p_1.style.display = "none";
+        p_1.style.transition = "0.5s";
+        
+    }
+
+    else{
+        p_1.style.transition = "1s";
+        p_1.style.display = "block";
+       
+        document.querySelector("#viñeta").className = "fa-solid fa-chevron-up";
+    }
+    
+
 
 })
+
+
+var boton2 = document.querySelector("#dash2");
+
+boton2.addEventListener("click" , () => {
+    
+    var p_2 = document.querySelector("#p-2");
+
+    if(p_2.style.display == "block"){
+
+        document.querySelector("#viñeta2").className = "fa-solid fa-angle-down";
+        p_2.style.display = "none"
+        
+    }
+    else{
+ 
+        p_2.style.display = "block"
+        document.querySelector("#viñeta2").className = "fa-solid fa-chevron-up";
+    }
+    
+
+
+})
+
+var boton3 = document.querySelector("#dash3");
+
+boton3.addEventListener("click" , () => {
+    
+    var p_3 = document.querySelector("#p-3");
+
+    if(p_3.style.display == "block"){
+
+        document.querySelector("#viñeta3").className = "fa-solid fa-angle-down";
+        p_3.style.display = "none"
+        
+    }
+    else{
+ 
+        p_3.style.display = "block"
+        document.querySelector("#viñeta3").className = "fa-solid fa-chevron-up";
+    }
+    
+
+
+})
+
+var boton1 = document.querySelector("#dash4");
+
+boton1.addEventListener("click" , () => {
+    
+    var p_4 = document.querySelector("#p-4");
+
+    if(p_4.style.display == "block"){
+
+        document.querySelector("#viñeta4").className = "fa-solid fa-angle-down";
+        p_4.style.display = "none"
+        
+    }
+    else{
+ 
+        p_4.style.display = "block"
+        document.querySelector("#viñeta4").className = "fa-solid fa-chevron-up";
+    }
+    
+
+
+})
+
+
+var boton1 = document.querySelector("#dash5");
+
+boton1.addEventListener("click" , () => {
+    
+    var p_5 = document.querySelector("#p-5");
+
+    if(p_5.style.display == "block"){
+
+        document.querySelector("#viñeta5").className = "fa-solid fa-angle-down";
+        p_5.style.display = "none"
+        
+    }
+    else{
+ 
+        p_5.style.display = "block"
+        document.querySelector("#viñeta5").className = "fa-solid fa-chevron-up";
+    }
+    
+
+
+})
+
+
+
+
+
+
+
+
+
